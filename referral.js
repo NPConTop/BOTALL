d2e0c828f4345b793bf6256626fa8f4ce131c753
NPConTop <abdullahsabar97@gmail.com>
2026-08-10 19:40:44 +0700

    [BlockMine] sync referral.js — 2026-08-10T12:40:43.859Z

--- referral.js
+++ referral.js
@@ -1,1 +1,79 @@
-{}
+{
+  "1342932763": {
+    "user_id": 1342932763,
+    "referredBy": null,
+    "referralPending": false,
+    "referralCount": 0,
+    "referralEarned": 0
+  },
+  "6080367121": {
+    "user_id": 6080367121,
+    "referredBy": null,
+    "referralPending": false,
+    "referralCount": 0,
+    "referralEarned": 0
+  },
+  "8488523937": {
+    "user_id": 8488523937,
+    "referredBy": null,
+    "referralPending": false,
+    "referralCount": 0,
+    "referralEarned": 0
+  },
+  "8884026662": {
+    "user_id": 8884026662,
+    "referredBy": null,
+    "referralPending": false,
+    "referralCount": 0,
+    "referralEarned": 0
+  },
+  "8331239982": {
+    "user_id": 8331239982,
+    "referredBy": null,
+    "referralPending": false,
+    "referralCount": 0,
+    "referralEarned": 0
+  },
+  "6971906523": {
+    "user_id": 6971906523,
+    "referredBy": null,
+    "referralPending": false,
+    "referralCount": 0,
+    "referralEarned": 0
+  },
+  "6638368874": {
+    "user_id": 6638368874,
+    "referredBy": null,
+    "referralPending": false,
+    "referralCount": 0,
+    "referralEarned": 0
+  },
+  "8690131842": {
+    "user_id": 8690131842,
+    "referredBy": null,
+    "referralPending": false,
+    "referralCount": 0,
+    "referralEarned": 0
+  },
+  "6988586493": {
+    "user_id": 6988586493,
+    "referredBy": null,
+    "referralPending": false,
+    "referralCount": 0,
+    "referralEarned": 0
+  },
+  "7739237719": {
+    "user_id": 7739237719,
+    "referredBy": null,
+    "referralPending": false,
+    "referralCount": 0,
+    "referralEarned": 0
+  },
+  "7256468442": {
+    "user_id": 7256468442,
+    "referredBy": null,
+    "referralPending": false,
+    "referralCount": 0,
+    "referralEarned": 0
+  }
+}
