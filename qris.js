dec186f345b9233000b8ba5c1a716439f35e9ac1
NPConTop <abdullahsabar97@gmail.com>
2026-08-10 20:09:29 +0700

    [BlockMine] sync qris.js — 2026-08-10T13:09:28.392Z

--- qris.js
+++ qris.js
@@ -1,15 +1,5 @@
 [
   {
-    "invoice_id": "6a7998e1036c5d336042703b",
-    "user_id": 1342932763,
-    "username": "𝐍𝐏𝐂 𝐆𝐎𝐃𝐇𝐀𝐍𝐃 𝘿' 𝙇𝙪𝙘𝙖̀ 𓇢𓆸",
-    "amount": 1000,
-    "status": "cancelled",
-    "created_at": 1786353891471,
-    "processed": true,
-    "cancelled_at": 1786353904480
-  },
-  {
     "invoice_id": "6a79abe8036c5d33604324df",
     "user_id": 6971906523,
     "username": "Madellxyz",
