d23f7d151dfbbc28ef28c3ee99e5e95f6254b260
NPConTop <abdullahsabar97@gmail.com>
2026-08-10 20:36:31 +0700

    [BlockMine] sync listerban.js — 2026-08-10T13:36:30.452Z

--- listerban.js
+++ listerban.js
@@ -1,10 +1,1 @@
-{
-  "1342932763": {
-    "id": 1342932763,
-    "username": "unknown",
-    "fullName": "Unknown",
-    "bannedAt": "10 Agustus 2026",
-    "permanent": true,
-    "expiresAt": null
-  }
-}
+{}
