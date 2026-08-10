17d1de3c5e968ebbda27666dc5cff74240325a88
NPConTop <abdullahsabar97@gmail.com>
2026-08-10 23:39:49 +0700

    [BlockMine] sync referral.js — 2026-08-10T16:39:48.741Z

--- referral.js
+++ referral.js
@@ -1,4 +1,14 @@
 {
+  "1342932763": {
+    "user_id": 1342932763,
+    "referredBy": 8331239982,
+    "referralPending": true,
+    "joinRewardAwarded": false,
+    "referralCount": 0,
+    "referralEarned": 0,
+    "topupRewardAwarded": false,
+    "topupRewardEarned": 0
+  },
   "6080367121": {
     "user_id": 6080367121,
     "referredBy": null,
