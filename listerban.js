7c08d070207e99edf38401061a20d57f90bcddd3
NPConTop <abdullahsabar97@gmail.com>
2026-08-11 11:28:07 +0700

    [BlockMine] sync listerban.js — 2026-08-11T04:28:07.394Z

--- listerban.js
+++ listerban.js
@@ -1,10 +1,1 @@
-{
-  "8884026662": {
-    "id": 8884026662,
-    "username": "wanntzyy12",
-    "fullName": "wann wan",
-    "bannedAt": "10 Agustus 2026",
-    "permanent": false,
-    "expiresAt": 1786397129767
-  }
-}
+{}
