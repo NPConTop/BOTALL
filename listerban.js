dfcdad0593d789d8e614391aa512d9ac00bb4643
NPConTop <abdullahsabar97@gmail.com>
2026-08-20 07:22:37 +0700

    [BlockMine] sync listerban.js — 2026-08-20T00:22:37.269Z

--- listerban.js
+++ listerban.js
@@ -1,10 +1,1 @@
-{
-  "7256468442": {
-    "id": 7256468442,
-    "username": "wannxyzx",
-    "fullName": "rdwnnzx",
-    "bannedAt": "19 Agustus 2026",
-    "permanent": true,
-    "expiresAt": null
-  }
-}
+{}
