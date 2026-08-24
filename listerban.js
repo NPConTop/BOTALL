393202acd26878552a01ed71813186018317a08c
NPConTop <abdullahsabar97@gmail.com>
2026-08-24 18:04:40 +0700

    [BlockMine] sync listerban.js — 2026-08-24T11:04:39.948Z

--- listerban.js
+++ listerban.js
@@ -1,1 +1,10 @@
-{}
+{
+  "6971906523": {
+    "id": 6971906523,
+    "username": "Madellxyz",
+    "fullName": "Ivixx",
+    "bannedAt": "24 Agustus 2026",
+    "permanent": true,
+    "expiresAt": null
+  }
+}
