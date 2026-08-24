5e1b6857c8e2ad713c2ab09607aaa6b7532a9fa0
NPConTop <abdullahsabar97@gmail.com>
2026-08-24 18:04:50 +0700

    [BlockMine] sync listerban.js — 2026-08-24T11:04:50.025Z

--- listerban.js
+++ listerban.js
@@ -6,5 +6,13 @@
     "bannedAt": "24 Agustus 2026",
     "permanent": true,
     "expiresAt": null
+  },
+  "6638368874": {
+    "id": 6638368874,
+    "username": "Skytwo",
+    "fullName": "Skytwo",
+    "bannedAt": "24 Agustus 2026",
+    "permanent": true,
+    "expiresAt": null
   }
 }
